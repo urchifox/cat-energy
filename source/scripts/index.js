498be3e137fc1beaf15205e36cb9a1af4647649e
urchifox <126313096+urchifox@users.noreply.github.com>
2024-01-19 14:28:46 +0400

    delete innessesary script

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,13 +3,3 @@
 import {init as initMenuToggle} from './menu-toggle.js';
 
 initMenuToggle();
-// let a;
-// const w = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
-// while(a = w.nextNode()){
-//   if(a.textContent.trim().length){
-//     // a.textContent = 'Одиннадцатиклассница пошла посмотреть на достопримечательность, она шла долго, несколько строчек, пока не пришла';
-//     // a.textContent = 'Одиннадцатиклассница получила неудовлетворительно и отправилась на водогрязеторфопарафинолечение от стресса.';
-//     // a.textContent = 'Одиннадцатиклассница отправилась на водогрязеторфопарафинолечение от стресса.';
-//     // a.textContent = 'Одиннадцатиклассница пошла на водогрязеторфопарафинолечение';
-//   }
-// }
